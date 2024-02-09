f2e92c9bec473b8b1dad5ade8165e2185dfb13c8
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-02-09 21:59:53 +0700

    fix: select batch index's shape

--- diffusion/model/diffusion/forward.py
+++ diffusion/model/diffusion/forward.py
@@ -8,12 +8,15 @@ class ForwardDiffusion(nn.Module):
         max_timesteps: int,
         beta_1: float = 0.0001,
         beta_2: float = 0.02,
+        dim: int = 32
     ):
         super().__init__()
-        self._beta_scheduler = self._linear_scheduler(max_timesteps, beta_1, beta_2)
-        alpha_cumprod = torch.cumprod(1 - self._beta_scheduler, dim=0)
-        self._sqrt_alpha = torch.sqrt(alpha_cumprod)
-        self._sqrt_1_minus_alpha = torch.sqrt(1 - alpha_cumprod)
+        self.max_timesteps = max_timesteps
+        self.dim = dim
+        self.beta_scheduler = self._linear_scheduler(max_timesteps, beta_1, beta_2)
+        self.alpha_cumprod = torch.cumprod(1 - self.beta_scheduler, dim=0)
+        self.sqrt_alpha = torch.sqrt(self.alpha_cumprod)
+        self.sqrt_1_minus_alpha = torch.sqrt(1 - self.alpha_cumprod)
 
     def _linear_scheduler(
         self,
@@ -31,7 +34,7 @@ class ForwardDiffusion(nn.Module):
         # x.shape = [T,]
         # t.shape = [B,]
         x_select = x.gather(dim=-1, index=t)
-        return x_select.unsqueeze(-1)  # [B,1]
+        return x_select.reshape(-1, 1, 1, 1)  # [B,1]
 
     def forward(
         self,
@@ -39,8 +42,8 @@ class ForwardDiffusion(nn.Module):
         t: torch.Tensor
     ):
         noise = torch.randn_like(x_0, device=x_0.device)
-        mean = self._batch_index_select(self._sqrt_alpha.to(x_0.device), t) * x_0
-        std = self._batch_index_select(self._sqrt_1_minus_alpha.to(x_0.device), t) * noise
+        mean = self._batch_index_select(self.sqrt_alpha.to(x_0.device), t) * x_0
+        std = self._batch_index_select(self.sqrt_1_minus_alpha.to(x_0.device), t) * noise
         return torch.clamp(mean + std, min=-1, max=1), noise
 
 
