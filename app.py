469e3afe47f2bc26ed7dc46f20173f42a922e603
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-02-15 19:00:05 +0700

    feat:
    add parser for demo

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 import torch
+import argparse
 import gradio as gr
 import diffusion
 from torchvision import transforms
 
-model = diffusion.DiffusionModel.load_from_checkpoint(
-    "./checkpoints/mnist.ckpt", in_channels=1, map_location='cpu', num_classes=10
-)
 
-to_pil = transforms.ToPILImage()
+parser = argparse.ArgumentParser()
+parser.add_argument("--ckpt_path", type=str, default="./checkpoints/mnist.ckpt")
+parser.add_argument("--map_location", type=str, default="gpu")
+parser.add_argument("--share", action='store_true')
+args = parser.parse_args()
 
+if __name__ == "__main__":
+    model = diffusion.DiffusionModel.load_from_checkpoint(
+        args.ckpt_path, in_channels=1, map_location=args.map_location, num_classes=10
+    )
+    to_pil = transforms.ToPILImage()
 
-def reset(image):
-    image = to_pil((torch.randn(1, 32, 32)*255).type(torch.uint8))
-    return image
+    def reset(image):
+        image = to_pil((torch.randn(1, 32, 32)*255).type(torch.uint8))
+        return image
 
+    def denoise(label):
+        labels = torch.tensor([label]).to(model.device)
+        for img in model.sampling_demo(labels=labels):
+            image = to_pil(img[0])
+            yield image
 
-def denoise(label):
-    labels = torch.tensor([label]).to(model.device)
-    for img in model.sampling_demo(labels=labels):
-        image = to_pil(img[0])
-        yield image
-
-
-def run():
     with gr.Blocks(theme=gr.themes.Soft(primary_hue="green")) as demo:
         gr.Markdown("# Simple Diffusion Model")
 
@@ -42,12 +46,8 @@ def run():
                 scale=2,
                 image_mode="L",
                 type='pil',
-             )
+            )
             sample_btn.click(denoise, [label], outputs=output)
             reset_btn.click(reset, [output], outputs=output)
-    return demo
 
-
-if __name__ == "__main__":
-    demo = run()
-    demo.launch()
+    demo.launch(share=args.share)
